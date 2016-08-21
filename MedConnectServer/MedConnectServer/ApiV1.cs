3b827cd1f08209aa60dc10885ad7e93dec2b892f
hindsight <hindsight@yandex.ru>
2016-08-21 17:03:30 +0300

    HelloWorld module

---
+++ MedConnectServer/MedConnectServer/ApiV1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace MedConnectServer {
+    public sealed class ApiV1 : NancyModule {
+        public ApiV1()
+            : base("/api/v1")
+        {
+            Get["/hello"] = _ => {
+                return new {
+                    Message = "Hello, world!"
+                };
+            };
+        }
+    }
+}
